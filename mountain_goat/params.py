25e28bd89e57302aab01ddb7bcd28a2e7aeb92a1
Zakaria Chahbar chahbar.zakaria@gmail.com <chahbar.zakaria@gmail.com>
2022-09-05 16:39:18 +0200

    ZCH_Setting up Cloud Storage

--- mountain_goat/params.py
+++ mountain_goat/params.py
@@ -3,6 +3,7 @@ from google.oauth2.service_account import Credentials
 
 PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(PACKAGE_DIR)
+RAW_DATA_DIR = os.path.join(BASE_DIR, "raw_data")
 
 # Credentials file
 CREDENTIAL_FILE = f"{BASE_DIR}/credentials.json"
